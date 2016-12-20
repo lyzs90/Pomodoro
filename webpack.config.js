564effb514b906f6d57c63b1c03b2a2c0a950949
lzys90 <leongyzs@gmail.com>
2016-12-21 00:36:16 +0800

    Add HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,27 @@
 var path = require('path');
+var webpack = require('webpack');
 
 var config = {
     context: path.join(__dirname, 'src'),
     entry: [
+        'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
         './main.js',
     ],
     output: {
         path: path.join(__dirname, 'www'),
         filename: 'bundle.js',
     },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
     module: {
         loaders: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loaders: ['babel'],
+                loaders: ['react-hot', 'babel'],
             },
         ],
     },
