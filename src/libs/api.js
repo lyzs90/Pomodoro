2ad241c5ef2cbbca4efa2c9eefbecae67fc9c5b9
lyzs90 <leongyzs@gmail.com>
2017-10-24 01:15:00 +0800

    Fix api bug

--- src/libs/api.js
+++ src/libs/api.js
@@ -1,17 +1,11 @@
-let url;
-
-if (process.env.NODE_ENV === 'production') {
-    url = `https://api.unsplash.com/photos/random/?collections=${collection}&client_id=${UNSPLASH_CLIENT_ID}`;
-}
-
 export const getImage = (collection) => {
-  return fetch(url, {
-      method: 'GET',
-      mode: 'cors',  // no-cors by default
-      headers: new Headers({
-          'Content-Type': 'application/json; charset=utf-8'
-      })
-  })
+    return fetch(`https://api.unsplash.com/photos/random/?collections=${collection}&client_id=${process.env.UNSPLASH_CLIENT_ID}`, {
+        method: 'GET',
+        mode: 'cors',  // no-cors by default
+        headers: new Headers({
+            'Content-Type': 'application/json; charset=utf-8'
+        })
+    })
     .then((response) => response.json())
     .then((image) => image.urls.full)
     .catch(err => {
